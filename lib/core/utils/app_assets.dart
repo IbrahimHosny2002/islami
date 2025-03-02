18f09a3ffe4ab91f465a6da9ab1e2bcbda954a16
IbrahimHosny2002 <hosnyhema66@gmail.com>
2025-03-02 22:07:12 +0200

    add app images

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,133 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesBackground
+  /// assets/images/background.png
+  static const String imagesBackground = "assets/images/background.png";
+
+  /// Assets for imagesGlow
+  /// assets/images/glow.png
+  static const String imagesGlow = "assets/images/glow.png";
+
+  /// Assets for imagesIcon1
+  /// assets/images/icon1.png
+  static const String imagesIcon1 = "assets/images/icon1.png";
+
+  /// Assets for imagesIcon10
+  /// assets/images/icon10.png
+  static const String imagesIcon10 = "assets/images/icon10.png";
+
+  /// Assets for imagesIcon11
+  /// assets/images/icon11.png
+  static const String imagesIcon11 = "assets/images/icon11.png";
+
+  /// Assets for imagesIcon12
+  /// assets/images/icon12.png
+  static const String imagesIcon12 = "assets/images/icon12.png";
+
+  /// Assets for imagesIcon13
+  /// assets/images/icon13.png
+  static const String imagesIcon13 = "assets/images/icon13.png";
+
+  /// Assets for imagesIcon14
+  /// assets/images/icon14.png
+  static const String imagesIcon14 = "assets/images/icon14.png";
+
+  /// Assets for imagesIcon15
+  /// assets/images/icon15.png
+  static const String imagesIcon15 = "assets/images/icon15.png";
+
+  /// Assets for imagesIcon16
+  /// assets/images/icon16.png
+  static const String imagesIcon16 = "assets/images/icon16.png";
+
+  /// Assets for imagesIcon17
+  /// assets/images/icon17.png
+  static const String imagesIcon17 = "assets/images/icon17.png";
+
+  /// Assets for imagesIcon18
+  /// assets/images/icon18.png
+  static const String imagesIcon18 = "assets/images/icon18.png";
+
+  /// Assets for imagesIcon2
+  /// assets/images/icon2.png
+  static const String imagesIcon2 = "assets/images/icon2.png";
+
+  /// Assets for imagesIcon3
+  /// assets/images/icon3.png
+  static const String imagesIcon3 = "assets/images/icon3.png";
+
+  /// Assets for imagesIcon4
+  /// assets/images/icon4.png
+  static const String imagesIcon4 = "assets/images/icon4.png";
+
+  /// Assets for imagesIcon5
+  /// assets/images/icon5.png
+  static const String imagesIcon5 = "assets/images/icon5.png";
+
+  /// Assets for imagesIcon6
+  /// assets/images/icon6.png
+  static const String imagesIcon6 = "assets/images/icon6.png";
+
+  /// Assets for imagesIcon7
+  /// assets/images/icon7.png
+  static const String imagesIcon7 = "assets/images/icon7.png";
+
+  /// Assets for imagesIcon8
+  /// assets/images/icon8.png
+  static const String imagesIcon8 = "assets/images/icon8.png";
+
+  /// Assets for imagesIcon9
+  /// assets/images/icon9.png
+  static const String imagesIcon9 = "assets/images/icon9.png";
+
+  /// Assets for imagesIslami
+  /// assets/images/Islami.png
+  static const String imagesIslami = "assets/images/Islami.png";
+
+  /// Assets for imagesLogo
+  /// assets/images/logo.png
+  static const String imagesLogo = "assets/images/logo.png";
+
+  /// Assets for imagesMosque1
+  /// assets/images/mosque1.png
+  static const String imagesMosque1 = "assets/images/mosque1.png";
+
+  /// Assets for imagesMosque2
+  /// assets/images/mosque2.png
+  static const String imagesMosque2 = "assets/images/mosque2.png";
+
+  /// Assets for imagesQuranBackground
+  /// assets/images/quran-background.png
+  static const String imagesQuranBackground = "assets/images/quran-background.png";
+
+  /// Assets for imagesShapeLeft
+  /// assets/images/shape-left.png
+  static const String imagesShapeLeft = "assets/images/shape-left.png";
+
+  /// Assets for imagesShapeRight
+  /// assets/images/shape-right.png
+  static const String imagesShapeRight = "assets/images/shape-right.png";
+
+  /// Assets for imagesSoundWave
+  /// assets/images/sound-wave.png
+  static const String imagesSoundWave = "assets/images/sound-wave.png";
+
+  /// Assets for imagesSplashScreen
+  /// assets/images/splash-screen.png
+  static const String imagesSplashScreen = "assets/images/splash-screen.png";
+
+  /// Assets for imagesTajMahalAgraIndia
+  /// assets/images/taj-mahal-agra-india.png
+  static const String imagesTajMahalAgraIndia = "assets/images/taj-mahal-agra-india.png";
+
+  /// Assets for imagesTajMahalAgraIndia2
+  /// assets/images/taj-mahal-agra-india2.png
+  static const String imagesTajMahalAgraIndia2 = "assets/images/taj-mahal-agra-india2.png";
+
+  /// Assets for imagesVerticalShotHassanIiMosqueCasablancaMorocco
+  /// assets/images/vertical-shot-hassan-ii-mosque-casablanca-morocco.png
+  static const String imagesVerticalShotHassanIiMosqueCasablancaMorocco = "assets/images/vertical-shot-hassan-ii-mosque-casablanca-morocco.png";
+}
+
